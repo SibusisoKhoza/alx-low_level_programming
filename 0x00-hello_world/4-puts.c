051319109685c931c7233c0495e2fe984843e53c
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-02-16 17:17:25 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
- * Return: Will return 0 if the program runss succefully
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
