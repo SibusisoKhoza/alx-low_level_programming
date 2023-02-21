19a28e94f6b3b61a73817e1b407aafcf41bfad1a
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-02-21 22:22:31 +0200

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
- *
+ * add - Entry point
+ * @a : first value
+ * @a : second value
  * Return: Always 0 (Success)
  */
- int add(int a, int b)
+int add(int a, int b)
 {
 	return (a + b);
 }
