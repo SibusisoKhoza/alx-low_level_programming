fd2285268e2a8cde22ce7f9887f81ba9f175323c
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-02-21 19:12:54 +0200

    Update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ void print_alphabet_x10(void)
+{
+	int i, j;
+
+	j = 0;
+
+	while (j < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		j++;
+		_putchar('\n');
+	}
+}
