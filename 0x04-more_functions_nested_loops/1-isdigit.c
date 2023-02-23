c5dc7f8899e76060d0b6ff584f755e05c6cfb2ec
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-02-23 22:30:13 +0200

    Update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isdigit - function
+ * @c: Charecter
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
