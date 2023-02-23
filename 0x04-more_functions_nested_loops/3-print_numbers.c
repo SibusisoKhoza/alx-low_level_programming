3578147fc9ab31baa49a68d463addcabc8f9c3b1
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-02-23 22:32:37 +0200

    Update 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Function
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
