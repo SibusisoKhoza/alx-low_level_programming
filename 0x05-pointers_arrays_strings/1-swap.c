0c5e9365b6eaab38536bb21661087de47bfa35cf
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-02-27 19:54:11 +0200

    Update 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - takes two pointers and swap their values
+ * @a : First value
+ * @b : Second value
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
