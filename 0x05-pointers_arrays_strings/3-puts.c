7dfe140441a7edb9bddb90000ae5d08281499b96
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-02-27 19:57:25 +0200

    Update 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - Prints a string and a new line
+ * @str: String to print
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
