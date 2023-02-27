296d984525bedb5dbd7a3633f32aa1345c782539
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-02-27 19:56:32 +0200

    Update 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - Checks for the length of a string
+ * @s : Points to the current char in an array
+ * Return: 0if success
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
