46a2a986ae004179542bee312559174256ea6dba
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-03-03 01:23:36 +0200

    Update 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - Print interger
+ * @n : Number
+ * Return: Returns void
+ */
+void print_number(int n)
+{
+	unsigned int size = n;
+
+	int digits = 0;
+
+	int i;
+
+	if (n < 0)
+		size = -n;
+	if (n == 0)
+		digits = 1;
+
+	while (size >= 1)
+	{
+		size = size / 10;
+		digits++;
+	}
+
+	for (i = 0; i < digits; i++)
+	{
+
+		int pow = 1;
+
+		int j;
+
+		int d;
+
+		for (j = 0; j < digits - i - 1; j++)
+		{
+			pow = pow * 10;
+		}
+
+		d = ((n / pow) % 10);
+		if (n < 0)
+		{
+			d = -d;
+			if (i == 0)
+				putchar(45);
+		}
+		putchar(48 + d);
+	}
+}
