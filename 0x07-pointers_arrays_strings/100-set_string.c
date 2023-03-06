032683fd7877a1c17308f90610ac974a3ca2a22f
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-03-06 19:55:24 +0200

    Update

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - Function
+ * @s : Source
+ * @to: String
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
