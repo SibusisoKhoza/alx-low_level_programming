2eb95b7e889221f8cfb9491a3835b14678e1cdf4
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-03-06 19:21:07 +0200

    Update

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - Memory copy function to fill another buffer
+ * @dest : String
+ * @src : String
+ * @n : Length
+ * Return: A string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
