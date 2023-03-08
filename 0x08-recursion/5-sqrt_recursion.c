04d04a43de2d4433ebebdc46963ae71d0b9de992
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-03-08 20:48:31 +0200

    Update

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * sqrt - Evaluantes 1 to n
+ * @a : Number
+ * @b : Number of iterations
+ * Return: On success
+ */
+int sqrt(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt(a, b + 1));
+}
+/**
+ * _sqrt_recursion - Funtion to get the square root of n
+ * @n : Number
+ * Return: On success
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
