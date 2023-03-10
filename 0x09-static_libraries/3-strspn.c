5bc6be81ec276414e5c067f9475dd9459d1bed8c
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-03-10 19:47:07 +0200

    Update

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn - Prints a char that exist in the first string
+ * @s : String
+ * @accept: Search string
+ * Return: A string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (j = 0; *(s + j); j++)
+	{
+		for (i = 0; *(accept + i); i++)
+		{
+			if (*(s + j) == *(accept + i))
+				break;
+		}
+	if (*(accept + i) == '\0')
+		break;
+	}
+	return (j);
+}
