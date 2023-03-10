f2357ef612de394481746d8f66a1387042f00e8f
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-03-10 20:38:58 +0200

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *_puts - prints a string
- * @str: string to print
- * Description: prints a string
- * On success: return the number of characters printed
+ * _puts - Prints a string and a new line
+ * @str: String to print
  */
 void _puts(char *str)
 {
-while (*str)
-_putchar(*str++);
-_putchar('\n');
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
 }
