f4526f099ead28143bde5881a5e9f67e4cc4df81
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-03-10 20:36:34 +0200

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,20 +1,24 @@
 #include "main.h"
-#include <stddef.h>
 /**
- *_strchr - Returns a pointer to the first occurrence
- *of the character c in the string s, or NULL if the
- *character is not found
- *@s:string targeted
- *@c:character targeted
- *Return: returns pointer to first occcurence of c
+ * _strchr - Prints from first char encountered
+ * @s : String
+ * @c : Char
+ * Return: A string
  */
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-;
-if (s[i] == c)
-return (s + i);
-else
-return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+
+		}
+		else
+			s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return (0);
 }
