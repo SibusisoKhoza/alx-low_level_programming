d450a3a194b855b7af268996b7174bb997a3b4a0
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-03-11 11:41:07 +0200

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,10 +10,9 @@
 int isInt(char *s)
 {
 	int i = 0;
-	
-	for(i = 0; s[i] != '\0'; i++)
+	for( i = 0; s[i] != '\0'; i++ )
 	{
-		if(s[i] < '0' || s[i] > '9')
+		if( s[i] < '0' || s[i] > '9' )
 			return (0);
 	}
 	return (1);
