f9fe958f65dcde3e6bf4fe1a85e602949bf282d3
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-03-23 04:31:44 +0200

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,36 +1,40 @@
 #include "3-calc.h"
+
 /**
- * main - Entry point
- * @argc: the number of the parameters
- * @argv: the parameters in the case the number to be calculated.
-(* a blank line
-* Description: this program is the enttry point for a calculator)?
-(* section header: 3-calc.h)*
-* Return: 0 in success
-*/
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-	int n1, n2, result;
-	int (*p)(int, int);
+	int a, b;
+	int (*operation)(int, int);
 
-	if (argc < 4 || argc > 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-
-	p = get_op_func(argv[2]);
-
-	if (p == NULL)
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = p(n1, n2);
 
-	printf("%d\n", result);
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
 	return (0);
 }
