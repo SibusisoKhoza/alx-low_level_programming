52a5efa1d0699dbffa33dc901e1d7959cf2d25b5
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-01 04:56:59 +0200

    Update main.h

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,15 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-/*
- * File: lists.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x11-singly_linked_lists directory.
- */
-
+#ifndef _LISTS_H_
+#define _LISTS_H_
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -17,7 +10,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *              for Holberton project
+ * for ALX project
  */
 typedef struct list_s
 {
@@ -25,11 +18,11 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-
+void  _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#endif /* LISTS_H */
+void tortoise(void) __attribute__ ((constructor));
+#endif
