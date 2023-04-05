41b6cb54b0894b8616adb2d603a7f0f6b35831c4
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-05 21:52:58 +0200

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,4 +32,6 @@ listint_t *reverse_listint(listint_t **head);
 listint_t *find_listint_loop(listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+int is_visited(const listint_t *, const listint_t **, int);
+listint_t *find_listint_loop(listint_t *head);
 #endif
