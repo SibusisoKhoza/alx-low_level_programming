3d45d282e3b42bfd80606a887c5999cafcb3b971
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-05 21:47:15 +0200

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_listint_safe - prints a list even with loop
@@ -8,72 +6,42 @@
  *
  * Return: number of nodes
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *node = head;
-	const listint_t **hash_table = NULL;
-	size_t size = 0;
-	size_t index = 0;
+	int count = 0;
+	const listint_t *visited[1024];
 
-	hash_table = malloc(sizeof(listint_t *) * 1024);
-	if (hash_table == NULL)
-		exit(98);
-
-	while (node != NULL)
+	while (head)
 	{
-		index = ((size_t)node) % 1024;
-		if (contains_node(hash_table, size, node))
+		if (is_visited(head, visited, count))
 		{
-			printf("-> [%p] %d\n", (void *)node, node->n);
+			printf("-> [%p] %d\n", (void *)head, head->n);
 			break;
 		}
-		add_node(hash_table, size, node);
-		printf("[%p] %d\n", (void *)node, node->n);
-		count++;
-		node = node->next;
+		visited[count++] = head;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-	free(hash_table);
 	return (count);
 }
 
 /**
- * contains_node - checks if a node is in the hash table
- * @hash_table: pointer to the hash table
- * @size: size of the hash table
- * @node: pointer to the node to check
+ * is_visited - check if a node is visited
+ * @node: pointer to node
+ * @visited: list of visited
+ * @count: length of visited
  *
- * Return: 1 if the node is in the hash table, 0 otherwise
+ * Return: 1 if is visited and 0 otherwise
  */
-int contains_node(const listint_t **hash_table, size_t size, const listint_t *node)
+int is_visited(const listint_t *node, const listint_t **visited, int count)
 {
-	size_t index = ((size_t)node) % size;
+	int i = 0;
 
-	while (index < size)
+	while (i < count)
 	{
-		if (hash_table[index] == node)
+		if (node == visited[i])
 			return (1);
-		index++;
+		i++;
 	}
 	return (0);
-}
-
-/**
- * add_node - adds a node to the hash table
- * @hash_table: pointer to the hash table
- * @size: size of the hash table
- * @node: pointer to the node to add
- */
-void add_node(const listint_t **hash_table, size_t size, const listint_t *node)
-{
-	size_t index = ((size_t)node) % size;
-
-	while (hash_table[index] != NULL)
-	{
-		if (hash_table[index] == node)
-			return;
-		index++;
-	}
-	hash_table[index] = node;
 }
