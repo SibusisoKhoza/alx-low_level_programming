b0ef291bf4189bbf521268a238519b1ed4b70442
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-05 19:04:46 +0200

    Update 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Print elements of a singly linked list.
+ * @h: Pointer to a list.
+ * Return: The number of nodes in the list.
+ **/
+size_t print_listint(const listint_t *h)
+{
+    const listint_t *current_node = h;
+    size_t node_count = 0;
+    
+    while (current_node != NULL)
+    {
+        printf("%d\n", current_node->n);
+        node_count++;
+        current_node = current_node->next;
+    }
+    
+    return node_count;
+}
