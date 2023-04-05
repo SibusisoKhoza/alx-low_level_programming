e11a6cc015c4e061a4cbd87507c546e21755959d
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-05 19:12:56 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,7 @@ size_t print_listint(const listint_t *h)
 {
 	const listint_t *current_node = h;
 	size_t node_count = 0;
-	
+
 	while (current_node != NULL)
 	{
 		printf("%d\n", current_node->n);
