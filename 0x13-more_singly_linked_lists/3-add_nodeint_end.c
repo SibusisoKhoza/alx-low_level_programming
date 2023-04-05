457519da888bd9e80ec97f0492fdf046b91238ac
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-05 20:15:14 +0200

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -34,7 +34,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (node);
 	}
 
-	for (last_node = *head; last_node->next != NULL; last_node = last_node->next);
+	for (last_node = *head; last_node->next != NULL; last_node = last_node->next)
+		;
 
 	last_node->next = node;
 
