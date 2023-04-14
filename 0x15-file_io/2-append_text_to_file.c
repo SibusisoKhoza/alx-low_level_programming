6aa22e06687f5f17ce48df4152b0fd6c05d01373
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-15 00:00:04 +0200

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	if (text_content == NULL)
@@ -36,15 +36,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	file_descriptor = open(filename, O_WRONLY | O_APPEND);
 	if (file_descriptor == -1)
 	{
-		return -1;
+		return (-1);
 	}
 
 	if (write(file_descriptor, text_content, length) == -1)
 	{
 		close(file_descriptor);
-		return -1;
+		return (-1);
 	}
 
 	close(file_descriptor);
-	return 1;
+	return (1);
 }
