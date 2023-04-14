430422db824a08c2278c0e14171a8662e59cac8d
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-14 23:26:13 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,39 +14,46 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    int file_descriptor, read_count, write_count;
-    char *buffer;
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor, read_count, write_count;
+	char *buffer;
 
-    buffer = (char*) malloc(letters);
-    if (buffer == NULL) {
-        return 0;
-    }
+	buffer = (char*) malloc(letters);
+	if (buffer == NULL)
+	{
+		return 0;
+	}
 
-    if (filename == NULL) {
-        return 0;
-    }
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1) {
-        free(buffer);
-        return 0;
-    }
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    read_count = read(file_descriptor, buffer, letters);
-    if (read_count == -1) {
-        free(buffer);
-        return 0;
-    }
 
-    write_count = write(STDOUT_FILENO, buffer, read_count);
-    if (write_count == -1) {
-        free(buffer);
-        return 0;
-    }
+	read_count = read(file_descriptor, buffer, letters);
+	if (read_count == -1)
+	{
+	free(buffer);
+		return (0);
+	}
 
-    close(file_descriptor);
-    free(buffer);
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+	if (write_count == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    return (write_count);
+	close(file_descriptor);
+	free(buffer);
+
+	return (write_count);
 }
