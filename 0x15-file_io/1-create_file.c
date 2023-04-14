8ace4f4ef3af1074677ae4a80c5dcb42b0a78546
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-14 23:49:50 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,35 +14,35 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int length = 0, file_descriptor;
+	int length = 0, file_descriptor;
 
-    if (filename == NULL)
-    {
-        return (-1);
-    }
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
-    if (text_content == NULL)
-    {
-        text_content = "";
-    }
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
 
-    while (text_content[length] != '\0')
-    {
-        length++;
-    }
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
 
-    file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (file_descriptor == -1)
-    {
-        return (-1);
-    }
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_descriptor == -1)
+	{
+		return (-1);
+	}
 
-    if (write(file_descriptor, text_content, length) == -1)
-    {
-        close(file_descriptor);
-        return (-1);
-    }
+	if (write(file_descriptor, text_content, length) == -1)
+	{
+		close(file_descriptor);
+		return (-1);
+	}
 
-    close(file_descriptor);
-    return (1);
+	close(file_descriptor);
+	return (1);
 }
