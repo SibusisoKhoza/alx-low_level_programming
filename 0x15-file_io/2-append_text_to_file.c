1dc9dc8d8a6538d3f853a7a78bffa7974f1a998e
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-14 23:52:58 +0200

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -10,28 +12,33 @@
  * Return: 1 on success, -1 on failure
  */
 
-int append_text_to_file(const char *filename, char *text_content)
-{
-	int i = 0, file;
+int append_text_to_file(const char *filename, char *text_content);
 
-	if (filename == NULL)
-		return (-1);
+int append_text_to_file(const char *filename, char *text_content) {
+    int length = 0, file_descriptor;
 
-	if (text_content == NULL)
-		text_content = "";
+    if (filename == NULL) {
+        return -1;
+    }
 
+    if (text_content == NULL) {
+        text_content = "";
+    }
 
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
+    while (text_content[length] != '\0') {
+        length++;
+    }
 
-	file = open(filename, O_WRONLY | O_APPEND);
+    file_descriptor = open(filename, O_WRONLY | O_APPEND);
+    if (file_descriptor == -1) {
+        return -1;
+    }
 
-	if (file == -1)
-		return (-1);
+    if (write(file_descriptor, text_content, length) == -1) {
+        close(file_descriptor);
+        return -1;
+    }
 
-	write(file, text_content, i);
-
-	return (1);
+    close(file_descriptor);
+    return 1;
 }
