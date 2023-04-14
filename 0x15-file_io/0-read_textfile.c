e0f1c44f238d95afde0ad8f8e4e83024a0125e22
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-14 23:04:59 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "holberton.h"
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) {
-        return 0;
-    }
+/**
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
+ */
 
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        return 0;
-    }
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file, let, w;
+	char *text;
 
-    char *buf = (char *) malloc(sizeof(char) * (letters + 1));
-    if (buf == NULL) {
-        close(fd);
-        return 0;
-    }
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
 
-    ssize_t bytes_read = read(fd, buf, letters);
-    if (bytes_read == -1) {
-        close(fd);
-        free(buf);
-        return 0;
-    }
+	if (filename == NULL)
+		return (0);
 
-    buf[bytes_read] = '\0';
-    ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read) {
-        close(fd);
-        free(buf);
-        return 0;
-    }
+	file = open(filename, O_RDONLY);
 
-    close(fd);
-    free(buf);
-    return bytes_read;
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
+	let = read(file, text, letters);
+
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (w);
 }
