1ad7ec33068a868e394f066a5474e709f67497ed
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-15 00:28:43 +0200

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,21 +18,15 @@ int main(int argc, char *argv[])
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	}
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (fd_to == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
 
 	while ((read_result = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
@@ -44,14 +38,10 @@ int main(int argc, char *argv[])
 	}
 
 	if (read_result == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	}
 
 	if (close(fd_from) == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-	}
 
 	if (close(fd_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
