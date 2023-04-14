1dffc375beb8e07624f8d64f7868213147b63439
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-04-14 23:46:14 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,40 +5,44 @@
 #include "main.h"
 
 /**
- * create_file - function that creates a file.
- * @filename: variable pointer
- * @text_content: content file
- * Description: Create a function that creates a file.
- * Return: 1 on success, -1 on failure
+ * create_file - Function
+ * @filename: Pointer
+ * @text_content: File
+ * Description: Create a file
+ * Return: 1 if success
  */
 
-int create_file(const char *filename, char *text_content);
-
-int create_file(const char *filename, char *text_content) {
+int create_file(const char *filename, char *text_content)
+{
     int length = 0, file_descriptor;
 
-    if (filename == NULL) {
-        return -1;
+    if (filename == NULL)
+    {
+        return (-1);
     }
 
-    if (text_content == NULL) {
+    if (text_content == NULL)
+    {
         text_content = "";
     }
 
-    while (text_content[length] != '\0') {
+    while (text_content[length] != '\0')
+    {
         length++;
     }
 
     file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (file_descriptor == -1) {
-        return -1;
+    if (file_descriptor == -1)
+    {
+        return (-1);
     }
 
-    if (write(file_descriptor, text_content, length) == -1) {
+    if (write(file_descriptor, text_content, length) == -1)
+    {
         close(file_descriptor);
-        return -1;
+        return (-1);
     }
 
     close(file_descriptor);
-    return 1;
+    return (1);
 }
