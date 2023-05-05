c10d93f00926bb70c7c323df70e0bb1dcef6ae95
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-05-05 23:48:12 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,28 +5,14 @@
  * Return: 0
  */
 unsigned int binary_to_uint(const char *b)
-{
-	unsigned int res = 0;
-	int base = 1, i = 0;
+unsigned int binary_to_uint(const char *b) {
+    if (b == NULL) return 0;
 
-	if (b == NULL)
-		return (0);
+    unsigned int decimal = 0;
+    for (int i = 0; b[i] != '\0'; i++) {
+        if (b[i] != '0' && b[i] != '1') return 0;
+        decimal = (decimal << 1) + (b[i] - '0');
+    }
 
-	while (b[i + 1])
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		i++;
-	}
-
-	while (i >= 0)
-	{
-		res += ((b[i] - '0') * base);
-		base *= 2;
-		i--;
-	}
-
-
-	return (res);
-
+    return (decimal);
 }
