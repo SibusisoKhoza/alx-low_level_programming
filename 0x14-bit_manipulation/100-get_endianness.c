c6041ef2b90a3167d4bd3551454fb0812f159708
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-05-06 00:58:38 +0200

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,18 +1,17 @@
 #include "main.h"
-/**
- * get_endianness - return the endianness of the machine
- *
- * Return: 0 for big endian, 1 for little endian
- */
-int get_endianness(void) {
-    int num = 1;
-    char *byte = (char *)&num;
 
-    // If the first byte of the integer contains the least significant bits,
-    // it means the system is little endian
-    if (*byte) {
-        return 1; // little endian
-    } else {
-        return 0; // big endian
-    }
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x;
+	char *c;
+
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
 }
