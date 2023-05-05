0a27e01bb71d1f6569e00b7f5d9b1e1d84e5fc35
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-05-06 00:29:04 +0200

    Update 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * binary_to_uint - Entry Point
+ * @b: const char
+ * Return: 0
+ */
+unsigned int binary_to_uint(const char *b) {
+	unsigned int result = 0;
+	int exponent = 1, index = 0;
+	
+	if (!b)
+		return (0);
+	
+	while (b[index + 1])
+	{
+		if (b[index] != '0' && b[index] != '1')
+			return (0);
+		index++;
+	}
+
+	while (index >= 0)
+	{
+		result += ((b[index] - '0') * exponent);
+		exponent *= 2;
+		index--;
+	}
+	
+	return (result);
+}
