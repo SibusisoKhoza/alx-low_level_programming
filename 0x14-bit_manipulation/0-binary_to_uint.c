b9f82d28a9c3409d7a6fbaa2094980d74c893d96
Sibusiso Khoza <51475597+SibusisoKhoza@users.noreply.github.com>
2023-05-06 00:15:26 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,13 +5,17 @@
  * Return: 0
  */
 unsigned int binary_to_uint(const char *b) {
-    if (b == NULL) return 0;
+    if (b == NULL) {
+        return 0; // return 0 if input string is NULL
+    }
 
     unsigned int decimal = 0;
     for (int i = 0; b[i] != '\0'; i++) {
-        if (b[i] != '0' && b[i] != '1') return 0;
-        decimal = (decimal << 1) + (b[i] - '0');
+        if (b[i] != '0' && b[i] != '1') {
+            return 0; // return 0 if any char in input string is not 0 or 1
+        }
+        decimal = (decimal << 1) + (b[i] - '0'); // convert binary digit to decimal equivalent
     }
 
-    return (decimal);
+    return decimal; // return decimal equivalent of binary input string
 }
