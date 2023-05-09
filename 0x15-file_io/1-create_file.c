a54ef4a20a28e49364c406800b747e3494024119
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-05-09 02:13:07 +0200

    Revert "Delete 0x15-file_io directory"
    This reverts commit 05dcd0c8a390e860dbf99f4b88eb8ac5da77771b.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_file - Function
+ * @filename: Pointer
+ * @text_content: File
+ * Description: Create a file
+ * Return: 1 if success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int length = 0, file_descriptor;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
+
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_descriptor == -1)
+	{
+		return (-1);
+	}
+
+	if (write(file_descriptor, text_content, length) == -1)
+	{
+		close(file_descriptor);
+		return (-1);
+	}
+
+	close(file_descriptor);
+	return (1);
+}
